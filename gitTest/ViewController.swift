c552d5f05f57f176c95e55a1f66e36a12b1d44cc
Muhammad Aqeel <muhammadaqeel@Muhammads-iMac.local>
2018-09-19 15:50:37 +0500

    eww

--- gitTest/ViewController.swift
+++ gitTest/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("from other")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
